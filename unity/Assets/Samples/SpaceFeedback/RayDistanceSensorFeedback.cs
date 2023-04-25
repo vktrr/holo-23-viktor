1b4fc451a34a45bbd774dca414d80405e9f5ad74
Mario von Rickenbach <mario@mariov.ch>
2023-04-25 18:51:33 +0200

    use both motors in space feedback scene

--- unity/Assets/Samples/SpaceFeedback/RayDistanceSensorFeedback.cs
+++ unity/Assets/Samples/SpaceFeedback/RayDistanceSensorFeedback.cs
@@ -12,13 +12,17 @@ public class RayDistanceSensorFeedback : MonoBehaviour
         {
             if (raycastTest.isHit)
             {
-                var speed = Mathf.InverseLerp(2, 0.5f, raycastTest.hitInfo.distance);
-                lastInputDevice.SendMotorSpeed(speed);
+                var speed0 = Mathf.InverseLerp(2, 0.5f, raycastTest.hitInfo.distance)*
+                             Mathf.InverseLerp(.2f, 0.6f, raycastTest.hitInfo.distance);;
+                var speed1 = Mathf.InverseLerp(.6f, 0.2f, raycastTest.hitInfo.distance);
+                lastInputDevice.SendMotorSpeed(0,speed0);
+                lastInputDevice.SendMotorSpeed(1,speed1);
             }
             else
             {
 
-                lastInputDevice.SendMotorSpeed(0);
+                lastInputDevice.SendMotorSpeed(0,0);
+                lastInputDevice.SendMotorSpeed(1,0);
             }
         }
     }
